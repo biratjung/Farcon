ac590be9813fbf0e919c20723787194a79a69f69
Birat Jung Thapa <biratjung@gmail.com>
2021-02-17 20:46:29 +0545

    database1

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,35 @@
-import express, { Router } from 'express'
-import asyncHandler from 'express-async-handler' 
-import Product from '../models/productModel.js'
+import express, { Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
 
-const router = express.Router()
+const router = express.Router();
 
 //@desc fetch all products
 //@route GET /api/products
 //@access Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
-
-
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 //@desc fetch single products
 //@route GET /api/products/:id
 //@access Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-    if(product){
-        res.json(product)
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not found' });
     }
-    else{
-        res.status(404).json({message: 'Product not found'})
-    }
-    res.json(product)
-}))
+    res.json(product);
+  })
+);
 
-export default Router
+export default router;
