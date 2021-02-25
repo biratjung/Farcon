6cebad89e08cb751bcb4af94a4476d82b60e4709
Birat Jung Thapa <biratjung@gmail.com>
2021-02-25 23:30:10 +0545

    redux

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs'
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true,
-    },
-    {
-        name: 'Usser',
-        email: 'usser@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    },
-    {
-        name: 'Userr',
-        email: 'userr@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    },
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Usser',
+    email: 'usser@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Userr',
+    email: 'userr@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
 export default users
