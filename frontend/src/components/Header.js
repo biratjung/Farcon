9ec5fd7f93485145416f93e662b265a4da1abc86
Birat Jung Thapa <biratjung@gmail.com>
2021-03-02 22:27:27 +0545

    navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
-    return (
-        <header>
-            <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <Navbar.Brand href="#home">FarCon</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                        <Nav.Link href="/cart">
-                            <i className='fas fa-shopping-cart'></i>Cart
-                            </Nav.Link>
-                        <Nav.Link href="/login">
-                        <i className='fas fa-user'></i>Sign In
-                        </Nav.Link>
-                        </Nav>
+  return (
+    <header>
+      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>FarCon</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
-}
-
-export default Header
+export default Header;
