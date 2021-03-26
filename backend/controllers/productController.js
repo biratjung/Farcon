69e5783e209d8ffdf5597e91ea821d906a753c67
Birat Jung Thapa <biratjung@gmail.com>
2021-03-26 22:39:50 +0545

    orderScreen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,7 +18,7 @@ const getProductById = asyncHandler(async (req, res) => {
     res.json(product);
   } else {
     res.status(404);
-    throw new encodeURIComponent('Product not found');
+    throw new Error('Product not found');
   }
 });
 
