b1c75a5e790b937081d2346f5f1fe9d03e393baf
Birat Jung Thapa <biratjung@gmail.com>
2021-05-09 20:41:38 +0545

    last

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className='text-center py-3'>
-                        Copyright &copy; FarCon
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            <a
+              href='mailto:biratjung@gmail.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              ContactUs with email
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
